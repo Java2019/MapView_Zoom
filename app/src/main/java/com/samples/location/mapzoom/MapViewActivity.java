4e8995cd29a4b00209f1fe22dec39132ebb799e5
java2019 <java_2019@mail.ru>
2017-07-02 14:50:03 +0300

    final

--- app/src/main/java/com/samples/location/mapzoom/MapViewActivity.java
+++ app/src/main/java/com/samples/location/mapzoom/MapViewActivity.java
@@ -2,33 +2,35 @@ package com.samples.location.mapzoom;
 
 import android.location.*;
 import android.os.Bundle;
-import android.widget.ImageView;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
-import com.google.android.maps.GeoPoint;
-import com.google.android.maps.MapActivity;
-import com.google.android.maps.MapController;
-import com.google.android.maps.MapView;
+import com.google.android.gms.maps.*;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.io.IOException;
 import java.util.List;
 import java.util.Locale;
 
-public class MapViewActivity extends MapActivity {
+public class MapViewActivity extends AppCompatActivity
+        implements OnMapReadyCallback{
 
     private static final int ZOOM_MAX = 21;
     private static final int ZOOM_INIT = 17;
 
-    private MapController controller;
     private TextView text;
     private TextView zoom;
-    private MapView map;
+    private SupportMapFragment map;
     private SeekBar seek;
+    private LatLng point;
+    private Location location;
 
     private LocationListener listener = new LocationListener() {
         @Override
-        public void onLocationChanged(Location location) {
+        public void onLocationChanged(Location loc) {
+            location = loc;
             printLocation(location);
         }
 
@@ -44,16 +46,15 @@ public class MapViewActivity extends MapActivity {
 
         @Override
         public void onProviderDisabled(String s) {
-            //printLocation(null);
+            printLocation(null);
         }
     };
 
     private SeekBar.OnSeekBarChangeListener seekBarListener = new SeekBar.OnSeekBarChangeListener() {
         @Override
         public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
-            int myZoomLevel = seekBar.getProgress() + 1;
-            controller.setZoom(myZoomLevel);
-            zoom.setText(String.valueOf(seek.getProgress() + 1));
+            zoom.setText(String.valueOf(seekBar.getProgress() + 1));
+            printLocation(location);
         }
 
         @Override
@@ -72,7 +73,9 @@ public class MapViewActivity extends MapActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map_view);
 
-        map = (MapView)findViewById(R.id.map);
+        map = (SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.map);
+        map.getMapAsync(this);
+
         text = (TextView)findViewById(R.id.text);
         zoom = (TextView)findViewById(R.id.zoom);
         seek = (SeekBar)findViewById(R.id.seekBar);
@@ -81,27 +84,16 @@ public class MapViewActivity extends MapActivity {
         seek.setProgress(ZOOM_INIT);
         zoom.setText(String.valueOf(ZOOM_INIT));
 
-        controller = map.getController();
-        controller.setZoom(ZOOM_INIT);
-
-        map.setSatellite(true);
-        // Добавляем элемент управления масштабированием карты
-        map.displayZoomControls(true);
-        map.setBuiltInZoomControls(true);
-
         LocationManager locationManager = (LocationManager)getSystemService(LOCATION_SERVICE);
         locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 2000, 10, listener);
-        printLocation(locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER));
+        location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        printLocation(location);
 
         // Подключаем обработчик события перемещения для ползунка
         seek.setOnSeekBarChangeListener(seekBarListener);
 
     }
 
-    @Override
-    protected boolean isRouteDisplayed() {
-        return false;
-    }
 
     public void printLocation(Location location){
         if (location != null){
@@ -109,18 +101,8 @@ public class MapViewActivity extends MapActivity {
             double lat = location.getLatitude();
             double lon = location.getLongitude();
 
-            GeoPoint geoPoint = new GeoPoint((int)(lat * 1E6), (int)(lon * 1E6));
-
-            controller.animateTo(geoPoint);
-
-            /*
-            ImageView marker = new ImageView(getApplicationContext());
-            marker.setImageResource(R.drawable.star);
-            MapView.LayoutParams markerParams = new MapView.LayoutParams(
-                    MapView.LayoutParams.WRAP_CONTENT, MapView.LayoutParams.WRAP_CONTENT,
-                    geoPoint, MapView.LayoutParams.TOP_LEFT );
-            map.addView(marker, markerParams);
-            */
+            point = new LatLng(lat, lon);
+            map.getMapAsync(this);
 
             // Отображаем координаты местоположения на экране
             text.setText(
@@ -149,5 +131,13 @@ public class MapViewActivity extends MapActivity {
         else {
             text.setText("Unabale get location");
         }
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        //googleMap.getUiSettings().setZoomControlsEnabled(true);
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(point, seek.getProgress() + 1));
+        googleMap.addMarker(new MarkerOptions().title("Vous ete ici").position(point));
+        //googleMap.animateCamera(CameraUpdateFactory.zoomTo(seek.getProgress() + 1), 2000, null);
     }
 }
